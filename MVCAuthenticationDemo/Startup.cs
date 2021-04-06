b459509d488361c1fd1a58dd439eb4c5b58641da
Saroj Adhikari <saroj.io@outlook.com>
2021-04-06 10:45:13 +0530

    Added MVC Authentication Demo - Base

---
+++ MVCAuthenticationDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAuthenticationDemo.Startup))]
+namespace MVCAuthenticationDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
